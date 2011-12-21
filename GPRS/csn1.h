7d7d8a9a76910748ac7d33ef97f2770b926e2689
Ivan Kluchnikov <kluchnikovi@gmail.com>
2011-12-21 13:17:53 +0300

    Moved CSN1 and RLC/MAC code to new GPRS directory.

--- GPRS/csn1.h
+++ GPRS/csn1.h
@@ -25,7 +25,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#include "BitVector.h"
+#include <BitVector.h>
 #include <iostream>
 #include <cstdlib>
 #ifndef _PACKET_CSN1_H_
